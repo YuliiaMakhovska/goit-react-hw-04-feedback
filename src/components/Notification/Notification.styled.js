d8957dd2221bfc6dd5a6e7df6e2c85ec392a1bff
YuliiaMakhovska <y.makhovska@gmail.com>
2022-12-25 09:23:33 +0200

    change styles

--- src/components/Notification/Notification.styled.js
+++ src/components/Notification/Notification.styled.js
@@ -1,6 +1,9 @@
 import styled from '@emotion/styled';
 
 export const NotificationText = styled.p`
+  font-family: inherit;
+  font-style: normal;
+  font-weight: 500;
   font-size: 25px;
   line-height: 1.15;
   color: tomato;
--- src/components/Statistics/Statistics.styled.js
+++ src/components/Statistics/Statistics.styled.js
@@ -1,14 +1,23 @@
 import styled from '@emotion/styled';
 
 export const StatisticsList = styled.ul`
-  background-color: #9feb9f;
+  color: #fff;
+  background-color: #3f51b5;
+  box-shadow: 0px 2px 4px -1px rgba(0, 0, 0, 0.2),
+    0px 4px 5px 0px rgba(0, 0, 0, 0.14), 0px 1px 10px 0px rgba(0, 0, 0, 0.12);
   border-radius: 5px;
   padding: 20px;
 `;
 
 export const StatisticsItem = styled.li`
-  display: flex;
-  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 16px;
+  padding-bottom: 24px;
   font-size: 20px;
   line-heigth: 1.15;
+  transition: all 250ms cubic-bezier(0.4, 0, 0.2, 1);
+  &:hover {
+    color: orange;
+  }
 `;
